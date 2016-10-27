8a29a074bfafaa20d05ce7db4a7ac7e2b85aef58
Pablo Carminatti <pabloc@oktana.io>
2016-10-27 17:44:59 -0300

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/post', function(req, res, next) {
+  res.send('respond with a resourcesss');
+});
+
+
+module.exports = router;
